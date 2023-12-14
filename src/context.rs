e399713724d55c60f1b19dcf0db582d982f07de9
zhkag <zhkag@foxmail.com>
2023-12-14 17:57:34 +0800

    线程切换可用

---
+++ src/context.rs
@@ -0,0 +1,103 @@
+
+use core::arch::asm;
+use crate::cpuport;
+
+#[export_name = "rt_hw_context_switch_to"]
+pub unsafe extern "C" fn rt_hw_context_switch_to(input:u32) {
+    asm!(".equ    SCB_VTOR,           0xE000ED08");
+    asm!(".equ    NVIC_INT_CTRL,      0xE000ED04");
+    asm!(".equ    NVIC_SYSPRI2,       0xE000ED20");
+    asm!(".equ    NVIC_PENDSV_PRI,    0xFFFF0000");
+    asm!(".equ    NVIC_PENDSVSET,     0x10000000");
+    asm!("mov r0, {}",in(reg) input);
+    asm!("LDR r1, ={}",sym cpuport::RT_INTERRUPT_TO_THREAD);
+    asm!("STR r0, [r1]");
+    asm!("MRS     r2, CONTROL");
+    asm!("BIC     r2, #0x04   ");
+    asm!("MSR     CONTROL, r2 ");
+    asm!("LDR r1, ={}",sym cpuport::RT_INTERRUPT_FROM_THREAD);
+    asm!("MOV r0, #0x0");
+    asm!("STR r0, [r1]");
+    asm!("LDR     r1, ={}",sym cpuport::RT_THREAD_SWITCH_INTERRUPT_FLAG);
+    asm!("MOV     r0, #1");
+    asm!("STR     r0, [r1]");
+    asm!("LDR r0, =NVIC_SYSPRI2");
+    asm!("LDR r1, =NVIC_PENDSV_PRI");
+    asm!("LDR.W   r2, [r0,#0x00]");
+    asm!("ORR     r1,r1,r2");
+    asm!("STR     r1, [r0]");
+    asm!("LDR r0, =NVIC_INT_CTRL");
+    asm!("LDR r1, =NVIC_PENDSVSET");
+    asm!("STR r1, [r0]");
+    asm!("LDR     r0, =SCB_VTOR");
+    asm!("LDR     r0, [r0]");
+    asm!("LDR     r0, [r0]");
+    asm!("NOP");
+    asm!("MSR     msp, r0");
+    asm!("CPSIE   F");
+    asm!("CPSIE   I");
+    asm!("DSB");
+    asm!("ISB");
+}
+
+
+
+#[export_name = "SysTick_Handler"]
+unsafe extern "C" fn sys_tick_handler() {
+
+}
+
+
+#[export_name = "PendSV_Handler"]
+unsafe extern "C" fn pend_sv_handler() {
+
+    asm!("MRS r2, PRIMASK");
+    asm!("CPSID   I");
+    
+    
+    loop {
+        asm!("NOP");
+    }
+}
+
+#[export_name = "SystemInit"]
+unsafe extern "C" fn system_init() {
+
+}
+
+#[no_mangle]
+fn __libc_init_array() {
+    //TODO link with newlib properly
+}
+
+#[export_name = "Reset_Handler"]
+unsafe extern "C" fn reset_handler() {
+    asm!("ldr sp, =_estack");
+    asm!("bl SystemInit");
+    asm!("ldr r0, =_sdata");
+    asm!("ldr r1, =_edata");
+    asm!("ldr r2, =_sidata");
+    asm!("movs r3, #0");
+    asm!("b 1f");
+    asm!("0:","ldr r4, [r2, r3]");
+    asm!("str r4, [r0, r3]");
+    asm!("adds r3, r3, #4");
+    asm!("1:");
+    asm!("adds r4, r0, r3");
+    asm!("cmp r4, r1");
+    asm!("bcc 0b");
+    asm!("ldr r2, =_sbss");
+    asm!("ldr r4, =_ebss");
+    asm!("movs r3, #0");
+    asm!("b 3f");
+    asm!("2:");
+    asm!("str  r3, [r2]");
+    asm!("adds r2, r2, #4");
+    asm!("3:");
+    asm!("cmp r2, r4");
+    asm!("bcc 2b");
+    asm!("bl __libc_init_array");
+    asm!("bl  entry");
+    asm!("bx  lr");
+}
+
